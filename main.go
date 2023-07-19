d110dc34b17bfdf14fd61cb521636a27dc5aa009
yqgs <gregsun1998@gmail.com>
2023-07-19 15:45:20 -0400

    Modified menus

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
+import (
+	"github.com/yqgs/Wisp/pkg/ui"
+)
+
 func main() {
 	// Code here
-
+	ui.WispInit()
 }
--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -2,81 +2,152 @@ package ui
 
 import (
 	"fmt"
-	"os"
+	"log"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{}
+type textmodel struct {
+	textInput textinput.Model
+	err       error
 }
 
-func initialModel() model {
-	return model{
-		choices:  []string{"foo", "bar", "baz"},
-		selected: make(map[int]struct{}),
-	}
+type model struct {
+	choices []string
+	cursor  int
 }
+
+type (
+	errMsg error
+)
 
 func (m model) Init() tea.Cmd {
 	return nil
 }
 
+func (m textmodel) Init() tea.Cmd {
+	return nil
+}
+
+func initialModel() model {
+	return model{
+		choices: []string{"Connect", "Host"},
+		cursor:  0,
+	}
+}
+
+func textModel() textmodel {
+	ti := textinput.NewModel()
+	ti.Focus()
+	ti.CharLimit = 20
+	ti.Width = 20
+
+	return textmodel{
+		textInput: ti,
+		err:       nil,
+	}
+}
+
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+
+	// Is it a key press?
 	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
 		switch msg.String() {
+
+		// These keys should exit the program.
 		case "ctrl+c", "q":
 			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
 		case "up", "k":
 			if m.cursor > 0 {
 				m.cursor--
 			}
+
+		// The "down" and "j" keys move the cursor down
 		case "down", "j":
 			if m.cursor < len(m.choices)-1 {
 				m.cursor++
 			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
 		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
+			if m.cursor == 0 {
+				return textModel(), nil
 			} else {
-				m.selected[m.cursor] = struct{}{}
+				return m, tea.Quit
 			}
 		}
 	}
 
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
 	return m, nil
 }
 
-func (m model) View() string {
-	s := "Use up/down to move the cursor and enter to select\n\n"
+func (m textmodel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
 
-	for i, choice := range m.choices {
-		cursor := " "
-		if m.cursor == i {
-			cursor = ">"
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+
+			return m, tea.Quit
 		}
 
-		checked := " "
-		if _, ok := m.selected[i]; ok {
-			checked = "x"
-		}
-
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
 	}
 
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+
+}
+
+func (m model) View() string {
+	s := "Wisp\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s %s\n", cursor, choice)
+	}
+
+	// The footer
 	s += "\nPress q to quit.\n"
 
+	// Send the UI for rendering
 	return s
+}
+
+func (m textmodel) View() string {
+	return fmt.Sprintf(
+		"Enter connection key\n\n%s\n\n%s",
+		m.textInput.View(),
+		"(esc to quit)",
+	) + "\n"
 }
 
 func WispInit() {
 	p := tea.NewProgram(initialModel())
-	if err := p.Start(); err != nil {
-		fmt.Printf("Error running program: %s\n", err)
-		os.Exit(1)
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
 	}
 }
